95398a0eb47edba797a26bfd236ebe1dac21a70a
Karen Simari <karensimari@live.com.ar>
2021-09-09 16:20:55 -0300

    Php form

---
+++ formulario.php
@@ -0,0 +1,29 @@
+<?php
+$destino = "info@krak.com.ar"; // Email del Sitio Web
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$telefono = $_POST["telefono"];
+$consulta = $_POST["mensaje"];
+$asunto = "Consulta :: Pagina Krak Inmobiliaria";
+
+$mensaje = "<html>\n";
+$mensaje .= "<body style='font-family:arial; font-size:14px;'>\n";
+$mensaje .= "<p style='background-color:#EEEEEE; padding:5px;'><b>" .$asunto ."</b></p>";
+$mensaje .= "<p><b>Nombre y Apellido:</b> " .$nombre ."<br>\n";
+$mensaje .= "<b>Email:</b> " .$email ."<br>\n";
+$mensaje .= "<b>Tel&eacute;fono/Celular:</b> " .$telefono ."<br>\n";
+$mensaje .= "<b>Consulta:</b> " .$consulta ."</p>\n";
+$mensaje .= "</body>\n";
+$mensaje .= "</html>\n";
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+$headers .= "From: " .$nombre ." <" .$email .">\r\n";
+$headers .= "Reply-To: " .$email ."\r\n";
+
+if (($nombre != "") && ($email != "")) {
+    mail($destino, $asunto, $mensaje, $headers);
+    header('Location: gracias.html');
+} else {
+	header('Location: error.html');
+}
+?>
